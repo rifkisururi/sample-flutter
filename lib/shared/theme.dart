c821d31cc7cb5447673f841e3982e2d9c6813910
Rifki Ahmad Sururi <rifkisururi27@gmail.com>
2024-03-09 08:08:57 +0700

    add theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color whiteColor = Color.fromARGB(255, 255, 255, 255);
+Color blackColor = Color.fromARGB(255, 0, 0, 0);
+Color greenColor = Color.fromARGB(255, 0, 255, 42);
+Color redColor = Color.fromARGB(255, 255, 0, 0);
+Color blueColor = Color.fromARGB(255, 0, 204, 255);
+Color greyColor = Color.fromARGB(255, 90, 90, 90);
