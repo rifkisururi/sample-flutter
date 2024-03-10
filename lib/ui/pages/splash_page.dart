ad3862ba99fd8df25903a20ab86380d759a4de59
Rifki Ahmad Sururi <rifkisururi27@gmail.com>
2024-03-10 10:06:06 +0700

    redirect to onboarding page after 2 second

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,9 +1,29 @@
+import 'dart:async';
+
 import 'package:app/shared/theme.dart';
+import 'package:app/ui/pages/onboarding_page.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget{
+class SplashPage extends StatefulWidget{
   const SplashPage({super.key});
-  
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 2), (){
+      Navigator.push(
+        context, MaterialPageRoute(
+          builder: (context) => const OnboardingPage()
+          )
+      );
+    });
+  }
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -23,5 +43,4 @@ class SplashPage extends StatelessWidget{
       )   
     );
   }
-
 }
