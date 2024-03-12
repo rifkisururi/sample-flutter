024b0b2db162db7bdd28a922f04e762c175be12d
Rifki Ahmad Sururi <rifkisururi27@gmail.com>
2024-03-13 05:11:41 +0700

    box button carosel

--- lib/ui/pages/onboarding_page.dart
+++ lib/ui/pages/onboarding_page.dart
@@ -34,6 +34,105 @@ class OnboardingPage extends StatelessWidget {
                 enableInfiniteScroll: false // biar bisa mentok gambar nya, tidak looping
               )
             ),
+            const SizedBox(
+              height: 80,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 24
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 22,
+                vertical: 24
+              ),
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    "Server H2h dflash \nMaju jatya",
+                    style: blackTextStyle.copyWith(
+                      fontSize: 20,
+                      fontWeight: semiBold
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  Text(
+                    'Dflash motto maju \nMaju terus',
+                    style: greenTextStyle.copyWith(
+                      fontSize: 16
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(
+                          right: 10
+                        ),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: blueColor
+                        ),
+                      ),
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(
+                          right: 10
+                        ),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: greyColor
+                        ),
+                      ),
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(
+                          right: 10
+                        ),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: greyColor
+                        ),
+                      ),
+                      const Spacer(), // mengisi ruang tersisa
+                      Container(
+                        width: 150,
+                        height: 50,
+                        child: TextButton(
+                          child: Text(
+                            'Lanjutkan',
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: semiBold
+                            ),
+                          ),
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            backgroundColor: blueColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(56)
+                            )
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
